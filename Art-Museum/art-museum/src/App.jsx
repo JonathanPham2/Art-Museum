0767ccbf9dcebfdc3d90c0319278be76673ff496
Jiamin Gao <tinabb1102@gmail.com>
2024-03-05 14:00:17 -0800

    phase1

--- Art-Museum/art-museum/src/App.jsx
+++ Art-Museum/art-museum/src/App.jsx
@@ -1,11 +1,21 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import harvardArt from './data/harvardArt';
-console.log(harvardArt)
+// console.log(harvardArt)
 
 const router =  createBrowserRouter([
   {
     path: "*",
     element: <h2>Page Not Found</h2>
+  },
+  {
+    path: '/',
+    element:<>
+    <h2>Harvard Art Museum</h2>
+    <p>
+  Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.
+    </p>
+</>
+
   }
 
 
@@ -16,7 +26,7 @@ function App() {
 
   return (
   <>
-  <h1> Hello from App </h1>
+  <h2> Hello from App </h2>
   <RouterProvider router = {router} />
 </>
   )
